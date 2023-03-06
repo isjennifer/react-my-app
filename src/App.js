58af602027092deb26d0123187afd02f31c1ea3b
김성하 <dundun@gimseonghaui-MacBookAir.local>
2023-03-06 21:07:01 +0900

    feat: map 함수로 데이터 화면에 출력하기

--- src/App.js
+++ src/App.js
@@ -4,17 +4,6 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
 
-// 로딩 후에 데이터를 가져오는 방법 1.
-  useEffect(() => {
-    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
-    .then((response) => response.json())
-    .then((json) => {
-      setMovies(json.data.movies)
-      setLoading(false)
-    })
-  }, [])
-
-// 로딩 후에 데이터를 가져오는 방법 2. (1과 같은 코드)
   const getMovies = async () => {
     const response = await fetch (`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`);
     const json = await response.json();
@@ -25,9 +14,21 @@ function App() {
     getMovies();
   }, [])
 
+
+  // map 함수를 쓸때에는 하위 element에 반드시 고유한 key를 붙여줘야 함.
   return (
     <div>
-      {loading ? <h1>Loading...</h1> : null}
+      {loading ? 
+        <h1>Loading...</h1> : 
+        <div>{movies.map((movie) => 
+          (<div key={movie.id}>
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            {movie.genres.map((g) => <li key={g}>{g}</li> )}
+          </div>)
+          )}
+        </div>
+      }
     </div>
   );
 }
