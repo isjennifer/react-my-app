6d4f6f2d883607e31a616f14ed2fd0e62222784c
김성하 <dundun@gimseonghaui-MacBookAir.local>
2023-03-06 20:47:35 +0900

    로딩후에 데이터를 가져오는 방법 두가지/ async, await, then 공부필요

--- src/App.js
+++ src/App.js
@@ -1,75 +1,33 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [value, setValue] = useState(0);
-  const [money, setMoney] = useState(0);
+  const [movies, setMovies] = useState([]);
 
+// 로딩 후에 데이터를 가져오는 방법 1.
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
+    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
     .then((response) => response.json())
-    .then((data) => {
-      setCoins(data)
-      setLoading(false);
+    .then((json) => {
+      setMovies(json.data.movies)
+      setLoading(false)
     })
   }, [])
 
-
-  const onChange = (event) => setValue(event.target.value);
-  const onChange2 = (event) => {
-    setMoney(event.target.selectedIndex)
+// 로딩 후에 데이터를 가져오는 방법 2. (1과 같은 코드)
+  const getMovies = async () => {
+    const response = await fetch (`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`);
+    const json = await response.json();
+    setMovies(json.data.movies)
+    setLoading(false)
   };
- // console.log(coins[money].quotes.)
-
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (value === 0) {
-      return;
-    }
-    setValue(0);
-  }
-
+  useEffect(() => {
+    getMovies();
+  }, [])
 
   return (
     <div>
-      <h1>The Coins! {loading ? null : `(${coins.length}ea)`}</h1>
-      <div>
-        <label htmlFor="id"><h3>How much do you have?</h3></label>
-        <form onSubmit={onSubmit}>
-          $<input
-            id="id"
-            type="number" 
-            value={value}
-            onChange={onChange}
-          />
-        </form>
-      </div>
-      <div>
-        <h3>What do you want to buy?</h3>
-        {loading ? 
-          <strong>loading...</strong> :
-          <select onChange={onChange2}>
-            {coins.map(
-              (item) => 
-                <option key={item.id} selectedindex={money}> 
-                  {item.name} ({item.symbol}): ${item.quotes.USD.price}USD 
-                </option>
-              )
-            }
-          </select>
-        }
-      </div>
-      <div>
-        <h3>
-          You can buy{" "}
-          {loading ? null : value / coins[money].quotes.USD.price}
-          {" "}
-          {loading ? null : coins[money].name}!
-        </h3>
-      </div>
+      {loading ? <h1>Loading...</h1> : null}
     </div>
   );
 }
