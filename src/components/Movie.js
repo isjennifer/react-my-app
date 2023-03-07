373f6ff37620a28087d10ee1d435286e7b8b42cd
김성하 <dundun@gimseonghaui-MacBookAir.local>
2023-03-07 22:11:45 +0900

    Docs: 2번줄경로  .추가

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 // Movie 함수에서 props로 부모의 것을 받아옴(coverImg, title, summary, genres를 부모의 것에서 받아옴)
 
 function Movie ({coverImg, title, summary, genres}) {
     return (<div>
     <img src={coverImg} alt={title}/>
-    <h2>{title}</h2>
+    <h2><Link to ="/movie">{title}</Link></h2>
     <p>{summary}</p>
     {genres.map((g) => <li key={g}>{g}</li>)}
     </div>);
