8c06b85b1f570bf699e821bbd35c6f61e1b00881
김성하 <dundun@gimseonghaui-MacBookAir.local>
2023-03-07 22:44:52 +0900

    useParams 사용하여 id 받기

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,8 @@
 import { useParams } from "react-router-dom";
 
 function Detail () {
-    const x = useParams();
-    console.log(x)
+    const { id } = useParams();
+    console.log(id)
     // console에는 해당 영화의 id가 뜬다. (url에 표시되는 id)
     return <h1>Detail</h1>
 }
