c94745054c7a6409f465fde90cccf01016338189
김성하 <dundun@gimseonghaui-MacBookAir.local>
2023-03-07 22:10:45 +0900

    feat: createBrowserRouter 사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
-
-function App() {
-  return null;
-}
+const App = createBrowserRouter([
+  { path: "/", element: <Home />, },
+  { path: "/movie", element: <Detail />, },
+]);
 
 export default App;
+
